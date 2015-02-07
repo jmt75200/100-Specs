d559573163eec4593875bb9a8f1c62999def5f33
jmt75200 <joelle@pcgamerzhawaii.com>
2015-02-06 14:15:55 -1000

    step 95 passes

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1443,8 +1443,8 @@ var closeAnOpenBox = new Box(new Animal('tiger'), true).openBox();
  * and assign the values to each variable below.
  *
  */
-var openAClosedDoor;
-var closeAnOpenDoor;
+var openAClosedDoor = new Door(false).openClose();
+var closeAnOpenDoor = new Door(true).openClose();
 
 
 /* Step 96
